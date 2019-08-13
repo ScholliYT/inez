3a4c81fc425c1ec7688f5ee735569901a84526bc
Nico Kranz <nico@kranz.space>
2019-08-13 22:16:22 +0200

    set up simple ef context

---
+++ INEZ/Data/Entities/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Azure.Search;
+
+namespace INEZ.Data.Entities
+{
+    public class Item
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [DisplayName("Name")]
+        [StringLength(30, MinimumLength = 3)]
+        [IsSearchable, IsFilterable, IsFacetable]
+        public string Name { get; set; }
+        [DisplayName("Beschreibung")]
+        [StringLength(100)]
+        [IsSearchable]
+        public string Description { get; set; }
+    }
+}
---
+++ INEZ/Data/InezContext.cs
@@ -0,0 +1,13 @@
+﻿using INEZ.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace INEZ.Data
+{
+    public class InezContext : DbContext
+    {
+        public InezContext(DbContextOptions<InezContext> options) : base(options)
+        { }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
