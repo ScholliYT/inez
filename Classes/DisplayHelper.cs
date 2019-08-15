4b59576951df47a161d3bcf2f66d0cfd456c3bd1
Scholli <scholliyt@gmail.com>
2019-08-16 00:44:02 +0200

    refactor GetDisplayName

---
+++ Classes/DisplayHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace INEZ.Classes
+{
+    public static class DisplayHelper
+    {
+        public static string GetDisplayName<TModel>(Expression<Func<TModel, object>> expression)
+        {
+            Type type = typeof(TModel);
+
+            string propertyName = null;
+            string[] properties = null;
+            IEnumerable<string> propertyList;
+            //unless it's a root property the expression NodeType will always be Convert
+            switch (expression.Body.NodeType)
+            {
+                case ExpressionType.Convert:
+                case ExpressionType.ConvertChecked:
+                    var ue = expression.Body as UnaryExpression;
+                    propertyList = (ue != null ? ue.Operand : null).ToString().Split(".".ToCharArray()).Skip(1); //don't use the root property
+                    break;
+                default:
+                    propertyList = expression.Body.ToString().Split(".".ToCharArray()).Skip(1);
+                    break;
+            }
+
+            //the propert name is what we're after
+            propertyName = propertyList.Last();
+            //list of properties - the last property name
+            properties = propertyList.Take(propertyList.Count() - 1).ToArray(); //grab all the parent properties
+
+            foreach (string property in properties)
+            {
+                PropertyInfo propertyInfo = type.GetProperty(property);
+                type = propertyInfo.PropertyType;
+            }
+
+            MemberInfo prop = type.GetProperty(propertyName);
+            if (prop.GetCustomAttribute(typeof(DisplayNameAttribute)) is DisplayNameAttribute dd)
+            {
+                return dd.DisplayName;
+            }
+            return null;
+        }
+    }
+}
