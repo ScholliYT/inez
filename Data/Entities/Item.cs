51bcb6a2e4d51ff4c2a9023d79fd3a55c58cf521
Scholli <scholliyt@gmail.com>
2019-08-25 02:29:19 +0200

    remove Microsoft.Azure.Search

--- Data/Entities/Item.cs
+++ Data/Entities/Item.cs
@@ -1,7 +1,7 @@
-﻿using System;
+using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using Microsoft.Azure.Search;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace INEZ.Data.Entities
 {
@@ -16,11 +16,9 @@ namespace INEZ.Data.Entities
         [Required]
         [DisplayName("Name")]
         [StringLength(30, MinimumLength = 3)]
-        [IsSearchable, IsFilterable, IsFacetable]
         public string Name { get; set; }
         [DisplayName("Beschreibung")]
         [StringLength(100)]
-        [IsSearchable]
         public string Description { get; set; }
     }
 }
